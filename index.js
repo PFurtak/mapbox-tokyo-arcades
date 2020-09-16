c882ec185edb0ddfb60a95568983f41160a8e13d
Patrick Furtak <patrick@patrickfurtak.com>
2020-09-16 03:54:33 -0400

    remove grid, rendered map to div and started styling

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('Hello World');
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicGZ1cnRhayIsImEiOiJja2YzbTFpeDQwNGJvMndrdWI1c2M3MDN4In0.7kXrdvI0M6BBO7uA--KwMA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/pfurtak/ckf4rthzl1jds19mtidpm7ndi',
+  center: [139.7536, 35.6834],
+  zoom: 10.7,
+});
+
+map.on('click', function (e) {
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['tokyo-arcades'],
+  });
+
+  if (!features.length) {
+    return;
+  }
+
+  var feature = features[0];
+
+  var popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat(feature.geometry.coordinates)
+    .setHTML(
+      '<a href=' +
+        feature.properties.url +
+        '><h3>' +
+        feature.properties.title +
+        '</a></h3><hr><h4>' +
+        feature.properties.address +
+        '</h4><p>' +
+        feature.properties.description +
+        '</p>'
+    )
+    .addTo(map);
+});
